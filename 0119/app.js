678c6982e5d0b430bcbedb70d6c972d14ebea2f2
Kwag De Gi <tech@skyaperture.com>
2015-01-19 04:17:43 +0900

    ko.computed done

--- 0119/app.js
+++ 0119/app.js
@@ -1,6 +1,9 @@
 function AppViewModel() {                                                                                                                                                                                              
 	this.firstName = ko.observable('Degi');                                                                                                                                                                                       
 	this.lastName = ko.observable("Kwag");                                                                                                                                                                                        
+	this.fullName = ko.computed(function() {
+		return this.firstName() + " " + this.lastName();
+	}, this);
 }                                                                                                                                                                                                                      
                                                                                                                                                                                                                                        
 ko.applyBindings(new AppViewModel()); 
